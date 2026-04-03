34180d5303613fc503710094c81fe616797a2e12
ray <raymondho.6472@gmail.com>
2026-04-03 22:20:17 +0000

    added audio functionallty and you can actually hear it no way

--- MusicPlayer/src/App.jsx
+++ MusicPlayer/src/App.jsx
@@ -2,7 +2,7 @@ import { MusicPlayer } from "./components/MusicPlayer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AllSongs } from "./components/AllSongs";
 import { Playlists } from "./components/Playlists";
-
+//1:31:00
 function App() {
   
 
--- MusicPlayer/src/components/MusicPlayer.jsx
+++ MusicPlayer/src/components/MusicPlayer.jsx
@@ -1,3 +1,94 @@
+import { useEffect, useRef } from "react";
+import { useMusic } from "../Hooks/useMusic";
+
 export const MusicPlayer = () => {
-    return <div></div>
+    const {currentTrack, 
+        formatTime, 
+        currentTime, 
+        setCurrentTime, 
+        duration, 
+        setDuration,
+        nextTrack,
+        prevTrack,
+        isPlaying,    
+        pause,
+        play,} = useMusic()
+    const audioRef = useRef(null);
+
+    const handleTimeChange = (e) => {
+        const audio = audioRef.current;
+        if (!audio) return;
+        const newTime = parseFloat(e.target.value);
+        audio.currentTime = newTime;
+        setCurrentTime(newTime);
+    }
+    useEffect(() => {
+        const audio = audioRef.current;
+        if (!audio) return;
+        if (isPlaying){
+            audio.play().catch((err) => consoler.error(err));
+        }else{
+            audio.pause();
+        }
+    }, [isPlaying])
+    useEffect(() => {
+        const audio = audioRef.current;
+
+
+        if (!audio) return;
+
+        const handleLoadedMetadata = () => {
+            setDuration(audio.duration)
+            
+        };
+        const handleTimeUpdate = () => {
+            setCurrentTime(audio.currentTime);
+        };
+        const handleEnded = () => {
+            nextTrack()
+        };
+        audio.addEventListener("loadedmetadata", handleLoadedMetadata)
+        audio.addEventListener("timeupdate", handleTimeUpdate)
+        audio.addEventListener("ended", handleEnded)
+
+
+        return () => {
+            audio.removeEventListener("loadedmetadata", handleLoadedMetadata)
+            audio.removeEventListener("timeupdate", handleTimeUpdate)
+            audio.removeEventListener("ended", handleEnded)
+
+
+        }
+
+    },[setDuration, setCurrentTime, currentTrack])
+
+    
+    return (
+    <div className="music-player">
+        <audio ref = {audioRef} src={currentTrack.url} preload = "metadata" crossOrigin="anonymous"/>
+    
+    <div className="track-info">
+        <h3 className="track-title">{currentTrack.title}</h3>
+            <p className="track-artist">{currentTrack.artist}</p>
+        
+    </div>
+    <div className="progress-container">
+        <span className="time">{formatTime(currentTime)}</span>
+        <input type="range" min="0" max={duration || 0} step= "0.1" value = {currentTime || 0}
+         className="progress-bar"
+         onChange ={handleTimeChange}
+         style = {{}} />
+        <span className="time">{formatTime(duration)}</span>
+    </div>
+    <div className="controls">
+        <button className="control-btn" onClick={prevTrack}> ⏮  </button>
+        <button className="control-btn play-btn" 
+            onClick= {() => (isPlaying ? pause() : play())}> 
+            {isPlaying ? "⏸" : "▶"} </button>
+        <button className="control-btn" onClick={nextTrack}> ⏭  </button>
+
+    </div>
+    
+        </div>
+    )
 }
